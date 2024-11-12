5aa0b8c47f30ecd1c08003f73c3c20ebe94b57b4
Sander <sandervdb23@gmail.com>
2024-11-12 22:42:32 +0700

    done

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const crypto = require("crypto");
-const secret = crypto.randomBytes(16).toString("hex");
+import { randomBytes } from "crypto";
+const secret = randomBytes(16).toString("hex");
 
 console.log(secret);
